8433048d20ec70fff0fad94b31a9c2a9d74d0eb2
João Alvim <joaoafonsoalvim@gmail.com>
2023-09-14 21:59:23 +0100

    blogs

---
+++ docs/blogs/music.md
@@ -0,0 +1,14 @@
+2023-14-09
+# Why not be immortal?
+
+À medida que o tempo passa  existe uma dessensibilização dos sentimentos.
+Quando uma pessoa se habitua aos sentimentos que vive eles vão ficando menos intensos.
+
+### O que aconteceria ao fim de 500 anos?
+As coisas tornar-se-iam repetitivas e previsíveis as Pessoas completamente desreguladas onde matar e torturar eram atos já sem significado?
+
+Temos que proibir nascimentos para prevenir crescimento da população, porque não há renovação das pessoas. Assim junto com as pessoas premaneceriam sempre as mesmas ideias e o conhecimento ficaria estagnado.
+
+### Qual é o sentido de vida de um ser imortal?
+
+Tenho a ideia de que quanto maior for a dificuldade de uma tarefa mair será a sensação de recompensa e de realização ao concretiza-la. Por isso o que seria uma tarefa difícil para um imortal? Esculpir um castelo do monte evareste com uma colher desafio
---
+++ docs/create-blog.py
@@ -0,0 +1,146 @@
+import os
+import re
+import markdown
+
+html ="""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <title>João Alvim</title>
+    <meta charset="utf-8">
+    <!-- ensures proper rendering for mobile -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library
+    <script
+      src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    -->
+    <!-- Latest compiled JavaScript
+    <script
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    -->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-default">
+
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.html">
+            <img src="img/rubiks2.png" alt="rubik" style="width:72%">
+          </a>
+        </div>
+        <ul class="nav navbar-nav">
+          <!-- <li><a href="index.html"><h3>about me</h3></a></li> -->
+          <li><a href="blog.html"><h3>blog</h3></a></li>
+          <li><a href="vitae.html"><h3>curriculum vitæ</h3></a></li>
+        </ul>
+      </div>
+    </nav>
+
+   
+    <div class="container">
+"""
+foot = """
+    </div>
+    <footer>
+      <div class="jumbotron">
+        <div class="container-fluid">
+          <div class="text-center">
+            <div class="mx-auto">
+              <!-- <h2>Contact Information</h2> -->
+
+              <p style="font-size: 16px;">github: <a
+                  href="https://github.com/jotaalvim">github.com/jotaalvim</a></p>
+              <p style="font-size: 16px;">email: <a
+                  href="mailto:joaoafonsoalvim@gmail.com">joaoafonsoalvim@gmail.com</a></p>
+              <p style="font-size: 16px;">location: Braga, Portugal</p>
+              <p style="font-size: 16px;">phone: +351 934662832</p>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </body>
+
+</html>
+"""
+
+directory_path = 'blogs/'
+
+try:
+    items = os.listdir(directory_path)
+    for file in items:
+        if file[-3:] != ".md":
+            items.remove(file)
+except:
+    items = []
+
+table_of_contents = []
+
+for file in items:
+    path = os.path.join('blogs',file)
+    with open(path, "r") as f:
+        file_content = f.read()
+
+    html_content = markdown.markdown(file_content)
+
+    # retirar data e título
+    lines = html_content.split('\n')
+    date  = lines.pop(0)
+    date = re.sub(r'<\s*p\s*>','<p style="font-size: 17px;">',date)
+
+    title = lines.pop(0)
+    lines.insert(0,title)
+    # COLOCAR OS TITULOS COM TAMANHO 2
+    title = re.sub(r'h\d','h2',title,2)
+
+    newhtml = '\n'.join(lines)
+
+    newhtml = re.sub(r'<\s*p\s*>','<p style="font-size: 16px;">',newhtml)
+
+    
+    blog_pos = html + newhtml + foot
+
+
+    newpath = os.path.join(directory_path,file[:-3]+'.html')
+    with open(newpath, "w") as wfile:
+        wfile.write(blog_pos)
+
+    table_of_contents.append((title,date,newpath))
+
+    print(date)
+    print(title)
+
+    print(table_of_contents)
+
+
+
+
+# fixme para depois
+#sorted(a, key=lambda x: x[1])
+
+
+
+
+
+post = """
+      <li>
+        <ul>
+          <a href="#"> <h2>Será a música um meta sentimento?</h2> </a>
+          <p style="font-size: 17px;">2023-09-14</p>
+        </ul>
+      </li>
+
+      <br>
+"""
+
